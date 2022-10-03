d9afb5124eab0d88cf6930e62aaea7485714b6b4
laurentbedu <bedulaurent@gmail.com>
2022-10-03 14:54:19 +0200

    TP Auth 3

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
+
 function LoginScreen() {
+
+    const [valid, setValid] = useState({email:false, password:false})
+    const validForm = (jsonData) => {
+        const isValid = {email:false, password:false}
+        const emailInput = document.getElementById("email-input");
+        const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if(emailPattern.test(emailInput.value)){
+            isValid.email = true;
+        }
+        const passwordInput = document.getElementById("password-input");
+        const passwordPattern = /^(?=.*[A-Z]).{6,}$/;
+        if(passwordPattern.test(passwordInput.value)){
+            isValid.password = true;
+        }
+        setValid(isValid);
+        return isValid.email === true && isValid.password === true;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const jsonData = Object.fromEntries(formData.entries()); 
         console.log(jsonData);
+        if(!validForm(jsonData)){
+            return;
+        }
         fetch('http://blog.api/auth/login', {
             method: "POST",
             body: JSON.stringify(jsonData)
@@ -12,33 +35,33 @@ function LoginScreen() {
         .then(json => {
             console.log(json);
         })
-
     }
 
   return (
     <>
       <h1>LoginScreen</h1>
-      <form className="w-50" onSubmit={handleSubmit}>
+      <form className="w-50" onSubmit={handleSubmit} noValidate>
         <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
+          <label htmlFor="email-input" className="form-label">
+            Email address <i className={"text-danger" + (valid.email ? " d-none":"")}>*</i>
           </label>
-          <input
+          <input 
             type="email" name="login"
             className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+            id="email-input"
           />
+          <i className={"text-danger" + (valid.email ? " d-none":"")}>* must be a valid email address</i>
         </div>
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Password
+          <label htmlFor="password-input" className="form-label">
+            Password <i className={"text-danger" + (valid.password ? " d-none":"")}>*</i>
           </label>
           <input
             type="password" name="password"
             className="form-control"
-            id="exampleInputPassword1"
+            id="password-input"
           />
+          <i className={"text-danger" + (valid.password ? " d-none":"")}>* 6 characters including a capital letter</i>
         </div>
 
         <button type="submit" className="btn btn-primary">

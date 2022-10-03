0e1134847fde1283d96cfa62daf46fac21bc8f02
laurentbedu <bedulaurent@gmail.com>
2022-10-03 14:21:48 +0200

    TP Auth 1

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,10 +1,17 @@
 function LoginScreen() {
 
+    
+
+    
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const jsonData = Object.fromEntries(formData.entries()); 
         console.log(jsonData);
+        
+
+
         fetch('http://blog.api/auth/login', {
             method: "POST",
             body: JSON.stringify(jsonData)
@@ -18,27 +25,28 @@ function LoginScreen() {
   return (
     <>
       <h1>LoginScreen</h1>
-      <form className="w-50" onSubmit={handleSubmit}>
+      <form className="w-50" onSubmit={handleSubmit} noValidate>
         <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
+          <label for="email-input" className="form-label">
+            Email address 
           </label>
-          <input
+          <input 
             type="email" name="login"
             className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+            id="email-input"
           />
+          
         </div>
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Password
+          <label for="password-input" className="form-label">
+            Password 
           </label>
           <input
             type="password" name="password"
             className="form-control"
-            id="exampleInputPassword1"
+            id="password-input"
           />
+          
         </div>
 
         <button type="submit" className="btn btn-primary">

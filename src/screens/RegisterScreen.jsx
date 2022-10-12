3ac5cd57567fd9b0d52b211d3c9d4e8ad7ce96a0
laurentbedu <bedulaurent@gmail.com>
2022-10-12 18:35:24 +0200

    Tp-Auth-11

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -12,6 +12,30 @@ export default function RegisterScreen() {
   return (
     <>
       <h1>Register</h1>
+      <form noValidate className="w-50"
+            onSubmit={handleSubmit(formSubmit, formInvalid)}>
+        <div className="mb-3">
+          <label htmlFor="pseudo-input" className="form-label">
+            Pseudo <i className={"text-danger"}>{errors.pseudo ? " *" : " " }</i>
+          </label>
+          <input id="pseudo-input" type="text" placeholder="pseudo"
+                {...register("pseudo", { required: true, minLength: 3 })} />
+          <i className={"text-danger d-block"}>{errors.pseudo ? "* at least 3 chars" : " " }</i>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email-input" className="form-label">
+            Email <i className={"text-danger"}>{errors.email ? " *" : " " }</i>
+          </label>
+          <input id="email-input" type="email" placeholder="email"
+                {...register("email", { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i })} />
+          <i className={"text-danger d-block"}>{errors.email ? "* must be a valid email address" : " " }</i>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </>
   );
 }
